2483955ecbf585674d71f6ff9ee38d3340f78dae
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-06-30 09:30:20 +0200

    Add heading

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,20 +1,27 @@
 //import "../List/List.css";
 export default function List({ activities, isGoodWeather, onDeleteActivity }) {
   return (
-    <ul className="activities_list">
-      {activities.map((activity) => {
-        return (
-          <li className="activity_item" key={activity.id}>
-            {activity.name}
-            <button
-              className="delete-btn"
-              onClick={() => onDeleteActivity(activity.id)}
-            >
-              ❌
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <h2>
+        {isGoodWeather
+          ? "The weather is awesome! Go outside and:"
+          : "Bad weather outside! Here's what you can do now:"}{" "}
+      </h2>
+      <ul className="activities_list">
+        {activities.map((activity) => {
+          return (
+            <li className="activity_item" key={activity.id}>
+              {activity.name}
+              <button
+                className="delete-btn"
+                onClick={() => onDeleteActivity(activity.id)}
+              >
+                ❌
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
